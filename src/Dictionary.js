d8c30fe54482d603bf90da3b89bdce5e41ffccce
Mayadev3 <107764065+Mayadev3@users.noreply.github.com>
2022-09-30 15:02:17 +0200

    added a state to target input

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Dictionary() {
+  let [keyWord, setKeyWord] = useState("null");
   function search(event) {
     event.preventDefault();
   }
 
+  function handleKeywordChange(event) {
+    event.preventDefault();
+    setKeyWord(event.target.value);
+    console.log(keyWord);
+  }
   return (
     <div className="Dictionary">
       <h1>Dictionary</h1>
       <form onSubmit={search}>
-        <input type="text" placeholder="Search for meaning..." />
+        <input
+          type="text"
+          placeholder="Search for meaning..."
+          onChange={handleKeywordChange}
+        />
         <i className="fa-solid fa-magnifying-glass search-icon"></i>
       </form>
     </div>

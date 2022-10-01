7cce978afc83247cb7f5fa8e56061c4123cc8f26
Mayadev3 <107764065+Mayadev3@users.noreply.github.com>
2022-10-01 19:08:33 +0200

    imported Axios and integrated the dictionary API

--- src/App.js
+++ src/App.js
@@ -12,6 +12,21 @@ export default function App() {
         </div>
         <Dictionary />
       </div>
+      <div className="text-audio">
+        <p>
+          <span className="label">
+            <strong>text</strong>
+          </span>{" "}
+          : "hə'ləʊ",
+        </p>
+        <p>
+          <span className="label">
+            <strong>audio</strong>
+          </span>
+          :
+          "//ssl.gstatic.com/dictionary/static/sounds/20200429/hello--_gb_1.mp3"
+        </p>
+      </div>
     </div>
   );
 }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyWord, setKeyWord] = useState("null");
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
+    //dictionary API source: https://dictionaryapi.dev/
     event.preventDefault();
     alert(`Searching for ${keyWord}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
+    axios.get(apiUrl).then(handleResponse);
+    console.log(apiUrl);
   }
 
   function handleKeywordChange(event) {
     event.preventDefault();
     setKeyWord(event.target.value);
   }
+
   return (
     <div className="Dictionary">
       <h1>Dictionary</h1>

d0c9314dfafaa56c19ad049f078f7032a3fc2148
Mayadev3 <107764065+Mayadev3@users.noreply.github.com>
2022-10-03 23:57:09 +0200

    looped through the synonyms and styled them

--- src/Results.js
+++ src/Results.js
@@ -45,21 +45,3 @@ export default function Results(props) {
     return null;
   }
 }
-/*   {props.results.phonetics.map(function (phonetic, index) {
-            return phonetic.audio;
-          })}
-        </div>
-        <div className="section-2">
-          <span className="label">
-            <strong>{props.results.meanings[0].partOfSpeech}</strong>
-          </span>
-          <p className="mt-2">
-            {props.results.meanings[0].definitions[0].definition}
-          </p>
-          <span className="label">
-            <strong>{props.results.meanings[1].partOfSpeech}</strong>
-          </span>
-          <p className="mt-2">
-            {props.results.meanings[1].definitions[0].definition}
-          </p>
-        </div>*/
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,6 +1,18 @@
 import React from "react";
-
+import "./Synonyms.css";
 export default function Synonyms(props) {
   console.log(props.synonyms);
-  return <div className="Synonyms"></div>;
+  return (
+    <div className="Synonyms">
+      {props.synonyms.map(function (synonym, index) {
+        return (
+          <div key={index}>
+            <ul>
+              <li className="synonymss">{synonym}</li>
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
 }

82083d0a19ae1e0ffc01457ef28d4d560fae561b
Mayadev3 <107764065+Mayadev3@users.noreply.github.com>
2022-10-04 16:42:56 +0200

    commented out the audio player

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning.js";
-import ReactAudioPlayer from "react-audio-player";
+//import ReactAudioPlayer from "react-audio-player";
 export default function Results(props) {
   console.log(props.results);
   if (props.results) {
